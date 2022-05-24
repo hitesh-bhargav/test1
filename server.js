632bdb0497c55e2fff86eb59984f4bccfe671f99
hitesh-bhargav <hitesh.bhargav@logixal.com>
2022-05-24 17:52:07 +0530

    auth api added

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use("/get", require("./getReports/getReports.controller.js"))
+app.use("/get", require("./reports/getReports.controller.js"))
+app.use("/auth", require("./auth/auth.controller.js"))
 
 // server port
 app.listen(9999, () => console.log("Server started on port 9999"));
